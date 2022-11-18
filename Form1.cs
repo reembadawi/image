7b61c2303875f0ac6a743fb91f702fb3bc1a04fc
reembadawi <116513774+reembadawi@users.noreply.github.com>
2022-11-18 13:23:27 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.ImageLocation = "https://www.google.com/intl/en_com/images/srpr/logo2w.png";
+        }
+    }
+}
